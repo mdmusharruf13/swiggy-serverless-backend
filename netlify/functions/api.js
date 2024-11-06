2d9c274fd5ee563a9101ede67f6cf414c3ab2970
musharruf <mdmusharruf13@gmail.com>
2024-11-06 22:53:29 +0530

    cors and env variables added

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -6,7 +6,7 @@ const router = Router();
 
 router.get("/main", async (req, res) => {
     try {
-        const apiResponse = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.3724&lng=78.4378&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING", {
+        const apiResponse = await fetch(process.env.BASE_URL, {
             headers: {
                 'Content-Type': 'application/json',
                 'User-Agent': 'MyApp/1.0'
@@ -18,7 +18,15 @@ router.get("/main", async (req, res) => {
         }
 
         const responseData = await apiResponse.json();
-        res.json(responseData);
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "https://mmfoods.netlify.app",
+                "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type",
+            },
+            body: JSON.stringify(responseData)
+        }
 
     } catch (error) {
         console.error("Fetch error: ", error);
